ba9215073a2882d7e8d58f00a0441acbb5a47a7f
Robert Young <rwy0717@gmail.com>
2024-04-30 15:18:21 -0400

    Clang format

--- include/circt/Dialect/FIRRTL/AnnotationDetails.h
+++ include/circt/Dialect/FIRRTL/AnnotationDetails.h
@@ -79,8 +79,7 @@ constexpr const char *testBenchDirAnnoClass =
     "sifive.enterprise.firrtl.TestBenchDirAnnotation";
 constexpr const char *moduleHierAnnoClass =
     "sifive.enterprise.firrtl.ModuleHierarchyAnnotation";
-constexpr const char *outputDirAnnoClass =
-    "circt.OutputDirAnnotation";
+constexpr const char *outputDirAnnoClass = "circt.OutputDirAnnotation";
 constexpr const char *declareOutputDirAnnoClass =
     "circt.DeclareOutputDirAnnotation";
 constexpr const char *testHarnessHierAnnoClass =
